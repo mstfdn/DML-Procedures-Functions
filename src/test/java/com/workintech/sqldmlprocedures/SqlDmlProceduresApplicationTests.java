30ae44fe6a8d4e259c7c6e8b36ea54e4b20968ce
Mustafa <mstf.fdn@outlook.com>
2025-02-17 15:59:44 +0300

    final

--- src/test/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplicationTests.java
+++ src/test/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplicationTests.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
